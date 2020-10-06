566567a6d674cea51da2438ae0cd19081289f788
Saikat Kanjilal <sxk1969@hotmail.com>
2020-10-05 19:24:47 -0700

    fixed tests by breaking up code into data retrieval and transforms

--- tests/postagger/reuters/test_reuters.py
+++ tests/postagger/reuters/test_reuters.py
@@ -1,6 +1,7 @@
 """A set of unit tests for the ohsumed data"""
 import unittest
 from nlpdata.postagger.reuters.reuters_pos_tagger import ReutersPosTagger
+import pandas as pd
 
 
 class TestReuters(unittest.TestCase):
@@ -11,9 +12,14 @@ class TestReuters(unittest.TestCase):
 
     def test_retrieve_data_sources_success(self):
         retrieved_data = self.reuters_post_obj.retrieve_upstream_data()
-        # we test a random assortment of keys
+        assert(len(list(retrieved_data)) > 0)
+
+    def test_transform_upstream_data_success(self):
+        retrieved_data = self.reuters_post_obj.retrieve_upstream_data()
         first_key = "2 NEW YORK BANK DISCOUNT WINDOW BORROWINGS 64 MLN DLRS IN FEB 25 WEEK Blah blah blah 3  "
-        assert(retrieved_data[first_key] == "Neg-")
+        transformed_data = self.reuters_post_obj.transform_upstream_data(retrieved_data)
+        tmp = transformed_data['class'].where(transformed_data['text'] == first_key)
+        assert (tmp[16] == 'Neg-')
 
     def test_retrieve_data_sources_failure(self):
         assert(1 == 1)
